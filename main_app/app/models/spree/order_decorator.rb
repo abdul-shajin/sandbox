61725625431720d16db35062017fa91648799262
Shajin <shajinvk@gmail.com>
2013-05-02 18:02:31 +0530

    checkout process

--- main_app/app/models/spree/order_decorator.rb
+++ main_app/app/models/spree/order_decorator.rb
@@ -1,14 +1,8 @@
-Spree::Order.class_eval do 
+Spree::Order.class_eval do
   checkout_flow do
       go_to_state :address
-      go_to_state :delivery #- Removed d delivery system
-      go_to_state :payment, if: ->(order) {
-        # Fix for #2191
-        if order.shipments
-          order.update_totals
-        end
-        order.payment_required?
-      }
+      #go_to_state :delivery #- Removed d delivery system
+      go_to_state :payment, :if => lambda { |order| order.payment_required? }
       go_to_state :confirm, if: ->(order) { order.confirmation_required? }
       go_to_state :complete, if: ->(order) {
         (order.payment_required? && order.has_unprocessed_payments?) || !order.payment_required?
